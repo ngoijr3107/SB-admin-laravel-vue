20aa0b3e454823eaa9f1d5ee522971186b6ba370
ngoijr3107 <pmizengo@gmail.com>
2022-08-27 04:23:52 +0300

    fist commit

---
+++ resources/js/views/admin/components/Sidebar.vue
@@ -0,0 +1,77 @@
+<template>
+    <ul
+      class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
+      <!-- Sidebar - Brand -->
+      <router-link
+        class="sidebar-brand d-flex align-items-center justify-content-center"
+        to="/"
+      >
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Statsociety</div>
+      </router-link>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0" />
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/admin">
+          <i class="fas fa-fw fa-home"></i>
+          <span>Dashboard</span></router-link
+        >
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider" />
+
+      <!-- Heading -->
+      <div class="sidebar-heading">MENUS</div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a
+          class="nav-link collapsed"
+          href="#"
+          data-toggle="collapse"
+          data-target="#collapseTwo"
+          aria-expanded="true"
+          aria-controls="collapseTwo"
+        >
+          <i class="fas fa-fw fa-cog"></i>
+          <span>USERS</span>
+        </a>
+        <div
+          id="collapseTwo"
+          class="collapse"
+          aria-labelledby="headingTwo"
+          data-parent="#accordionSidebar"
+        >
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <router-link class="collapse-item" to="/admin">Add user</router-link>
+            <router-link class="collapse-item" to="/admin">All users</router-link>
+          </div>
+        </div>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider" />
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+    </ul>
+    <!-- End of Sidebar -->
+</template>
+
+<script>
+
+export default {
+    name: 'Sidebar'
+}
+</script>
